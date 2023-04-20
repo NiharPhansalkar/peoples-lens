dd011b5214dcbecd86ce1cbb9c7661097a6470e9
Nihar Phansalkar <phansalkarnihar@gmail.com>
2023-04-20 06:07:18 +0000

    added correct recognition details

--- recognize_user/js/makeVideo.js
+++ recognize_user/js/makeVideo.js
@@ -23,7 +23,7 @@ async function getLabeledFaceDescriptions(labels) {
         return Promise.all(
         labels.map(async (label) => {
             const descriptions = [];
-            const downloadUrl = await fetch('/recognize_user/downloadLink');
+            const downloadUrl = await fetch(`/recognize_user/downloadLink?label=${label}`);
             const image = await faceapi.fetchImage(downloadUrl);
             const detections = await faceapi
                 .detectSingleFace(image)
