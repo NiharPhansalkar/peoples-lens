ee4ad1523a9dc0a931d85d35e5deaef87c1435ce
Nihar Phansalkar <phansalkarnihar@gmail.com>
2023-04-22 05:04:14 +0000

    added file path

--- server_files/main.js
+++ server_files/main.js
@@ -24,7 +24,7 @@ admin.initializeApp({
 });
 
 const storage = new Storage({
-    keyFilename: '../peopleslens-pbl-firebase-adminsdk-pohc3-6f89177c29.json'
+    keyFilename: path.join(path.resolve(__dirname, "../"), '/peopleslens-pbl-firebase-adminsdk-pohc3-6f89177c29.json')
 });
 
 const port = process.env.PORT || 3000;
